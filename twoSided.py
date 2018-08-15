20d156848ec3d3a46907ecd260e7c6c771934d77
hexidian <leokastenberg@gmail.com>
2018-08-15 11:14:31 -0400

    polish up program

--- twoSided.py
+++ twoSided.py
@@ -4,6 +4,8 @@
 
 from math import sqrt
 
+MAXLENGTH = 9
+
 def isPrime(n):
     #a pretty rudamentary prime checking function. It's O(sqrt(n)), so it works pretty well.
     if n < 2:
@@ -19,12 +21,17 @@ def expand(n):#this is the core function of this program. It is recursive.
 
     ns = str(n) #simply the string verison of n
 
-    print "working on",n
-    if len(ns) > 8: #for now we will only look at short ones, so that it doesn't crash my computer
+    if len(ns) < 6:#this way we only print it when we tick over the smaller numbers, which is like ticking over a big digit if that makes sense
+        print "working on",n
+
+    if len(ns) > MAXLENGTH: #for now we will only look at short ones, so that it doesn't crash my computer
         return []
+        #anything that returns a [] shall indicate that there were no dead ends before the length limit
     found = []
 
-    for i in range(1,10,2):#we only need try adding odd numbers, because any number ending with an even number is going to be non-prime (obviously except for 2 itself)
+    stupidMarkerThatShouldHaveABetterNameButImLazy = False
+
+    for i in [1,3,7,9]:#we only need try adding odd numbers, because any number ending with an even number is going to be non-prime (obviously except for 2 itself). We also don't need to check 5 because the number will be a multiple of 5
         ns += str(i)
         for j in range(1,10):#we can't make the same simplification as before because this number is going to be appended as the most significant digit
             new = str(j)
@@ -36,14 +43,18 @@ def expand(n):#this is the core function of this program. It is recursive.
                     #if there are no deeper things to be found, then this number is a deadend and should be added to the list because it can no longer be expanded
                     found.append(new)
                     print "I found:",new
-                else:
+                    stupidMarkerThatShouldHaveABetterNameButImLazy = True
+                elif deeperFound != []:
                     found.extend(deeperFound)
+                    stupidMarkerThatShouldHaveABetterNameButImLazy = True
         ns = ns[:-1]    #takes off the digit we just tested on the end
 
-    return (found if found != [] else None)
+    return [] if stupidMarkerThatShouldHaveABetterNameButImLazy == False else found if found != [] else None #what a python line
 
 def main():
-    print expand(5)#just a test number. The program should run with a non-prime number, but the results will only be meaningful if the seed is prime.
-
+    found = []
+    for i in [2,3,5,7]:
+        found.extend(expand(i))#just a test number. The program should run with a non-prime number, but the results will only be meaningful if the seed is prime.
+    print found
 if __name__ == "__main__":
     main()
